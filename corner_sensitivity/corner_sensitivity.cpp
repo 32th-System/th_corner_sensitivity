8b3c73b79cceada991650b5cfb2909e8bbf9c25c
32th System <intensonat@gmail.com>
2019-10-12 17:18:36 +0200

    Make it actually work

--- corner_sensitivity/corner_sensitivity.cpp
+++ corner_sensitivity/corner_sensitivity.cpp
@@ -6,6 +6,8 @@
 #include <math.h>
 #include <malloc.h>
 
+const wchar_t *default_ini = L"[main]\n; corner_sensitivity: 0 through 90 degrees. 0 = no corners, 90 = everything is a corner\ncorner_sensitivity = 50\n";
+
 DWORD(__stdcall *__XInputGetState)(DWORD, XINPUT_STATE*) = nullptr;
 
 float deadzone = 0;
@@ -13,23 +15,70 @@ float corner_sensitivity = 0;
 double min_sens = 0;
 double max_sens = 0;
 
+DWORD __stdcall _XInputGetState(DWORD dwUserIndex, XINPUT_STATE *pState) {
+	bool corner_allowed = true;
+	DWORD ret = __XInputGetState(dwUserIndex, pState);
+
+
+	if (ret != ERROR_SUCCESS) {
+		return ret;
+	}
+
+	double push = sqrt(pState->Gamepad.sThumbLX*pState->Gamepad.sThumbLX + pState->Gamepad.sThumbLY*pState->Gamepad.sThumbLY);
+
+	if (push < 10000) { // Same as ZUN's hardcoded deadzone of 10000
+		pState->Gamepad.sThumbLX = 0;
+		pState->Gamepad.sThumbLY = 0;
+		return ERROR_SUCCESS;
+	}
+
+	SHORT posX = pState->Gamepad.sThumbLX;
+	SHORT posY = pState->Gamepad.sThumbLY;
+
+	if (0 > posX) {
+		if (posX = -32768) {
+			posX = 32767;
+		}
+		else {
+			posX = -posX;
+		}
+	}
+	if (0 > posY) {
+		if (posY = -32768) {
+			posY = 32767;
+		}
+		else {
+			posY = -posY;
+		}
+	}
+
+	double angle = atan2(posY, posX) * (180/PI);
+
+	if (!((angle - max_sens)*(angle - min_sens) <= 0)) {
+		posX > posY ? pState->Gamepad.sThumbLY = 0 : pState->Gamepad.sThumbLX = 0;
+	}
+
+	return ERROR_SUCCESS;
+}
+
 int __stdcall thcrap_plugin_init() {
-	__XInputGetState = (DWORD(__stdcall*)(DWORD, XINPUT_STATE*))GetProcAddress(GetModuleHandle(L"xinput1_3.dll"), "XInputGetState");
+	DWORD tmp_get_state = *(DWORD*)0x0049A278;
+	__XInputGetState = (DWORD(__stdcall*)(DWORD, XINPUT_STATE*))tmp_get_state;
+
 	size_t path_len = (GetCurrentDirectoryW(0, NULL) + 24) * sizeof(wchar_t); // 22 = wcslen(L"corner_sensitivity.ini") + 2 (NULL terminator and \)
 	wchar_t *path = (wchar_t*)_alloca(path_len);
 	GetCurrentDirectoryW(path_len, path);
 	wcscpy(PathAddBackslashW(path), L"corner_sensitivity.ini");
-	wchar_t deadzone_str[6];
-	wchar_t corner_sensitivity_str[3];
-	GetPrivateProfileStringW(L"main", L"deadzone", L"600", deadzone_str, 6, path);
-	GetPrivateProfileStringW(L"main", L"corner_sensitivity", L"30", corner_sensitivity_str, 3, path);
-	wchar_t *end;
-	deadzone = wcstof(deadzone_str, &end);
-	corner_sensitivity = wcstof(corner_sensitivity_str, &end);
-	if (deadzone == 0.0) {
-		MessageBoxW(*((HWND*)0x5226C0), L"The deadzone value in corner_sensitivity.ini is invalid, using default value!", L"Error", MB_ICONERROR | MB_OK);
-		deadzone = 600.0;
+	if (!PathFileExistsW(path)) {
+		HANDLE hIni = CreateFileW(path, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+		DWORD nByteW;
+		WriteFile(hIni, (LPCVOID)default_ini, wcslen(default_ini) * sizeof(wchar_t), &nByteW, NULL);
+		CloseHandle(hIni);
 	}
+	wchar_t corner_sensitivity_str[3];
+	GetPrivateProfileStringW(L"main", L"corner_sensitivity", L"50", corner_sensitivity_str, 3, path);
+	wchar_t *end;
+	corner_sensitivity = wcstof(corner_sensitivity_str, &end);
 	if (corner_sensitivity == 0.0) {
 		MessageBoxW(*((HWND*)0x5226C0), L"The corner_sensitivity value in corner_sensitivity.ini is invalid, using default value!", L"Error", MB_ICONERROR | MB_OK);
 		corner_sensitivity = 30.0;
@@ -37,59 +86,11 @@ int __stdcall thcrap_plugin_init() {
 	min_sens = (90 - corner_sensitivity) / 2;
 	max_sens = min_sens + corner_sensitivity;
 	_freea(path);
+
+	DWORD OldProtect;
+	VirtualProtect((LPVOID)0x49A000L, 0x13000L, PAGE_READWRITE, &OldProtect);
+	*(DWORD*)0x0049A278 = (DWORD)&_XInputGetState;
+	VirtualProtect((LPVOID)0x49A000L, 0x13000L, OldProtect, &OldProtect);
+
 	return 0;
-}
-
-DWORD _XInputGetState(DWORD dwUserIndex, XINPUT_STATE *pState) {
-
-	bool corner_allowed = true;
-
-	DWORD ret = __XInputGetState(dwUserIndex, pState);
-
-	
-	if (ret != ERROR_SUCCESS) {
-		return ret;
-	}
-		
-	double push = sqrt(pState->Gamepad.sThumbLX*pState->Gamepad.sThumbLX + pState->Gamepad.sThumbLY*pState->Gamepad.sThumbLY);
-
-	if (push < deadzone) {
-		pState->Gamepad.sThumbLX = 0;
-		pState->Gamepad.sThumbLY = 0;
-		return ERROR_SUCCESS;
-	}
-	
-	SHORT posX;
-	SHORT posY;
-
-	if (pState->Gamepad.sThumbLX >> 15) {
-		posX = -pState->Gamepad.sThumbLX;
-	}
-	else {
-		posX = pState->Gamepad.sThumbLX;
-	}
-
-	if (pState->Gamepad.sThumbLY >> 15) {
-		posY = -pState->Gamepad.sThumbLY;
-	}
-	else {
-		posY = pState->Gamepad.sThumbLY;
-	}
-
-	double angle = atan2(posY, posX) * PI / 180;
-
-	if (!((angle - min_sens) <= (max_sens - min_sens))) {
-		corner_allowed = false;
-	}
-	
-	BYTE sign = (pState->Gamepad.sThumbLX >> 15);
-	posX > posY ? pState->Gamepad.sThumbLX = 32767 : pState->Gamepad.sThumbLY = 32767;
-	if (corner_allowed = true) {
-		posX > posY ? pState->Gamepad.sThumbLY = 32767 : pState->Gamepad.sThumbLX = 32767;
-	}
-	else {
-		posX > posY ? pState->Gamepad.sThumbLY = 0 : pState->Gamepad.sThumbLX = 0;
-	}
-
-	return ret;
 }
